053ad9681ac0c856bf129dcd0122ca1d2c72353f
saulocapistrano <saulocapistrano@gmail.com>
2025-12-28 22:18:04 -0300

    feat(kafka): refactor KafkaEventPublisher to support publishing SolicitacaoCreditoAnalisadaEvent and update existing ConsultaCreditoEvent handling

--- src/main/java/br/com/searchcredit/infrastructure/kafka/KafkaEventPublisher.java
+++ src/main/java/br/com/searchcredit/infrastructure/kafka/KafkaEventPublisher.java
@@ -1,6 +1,7 @@
 package br.com.searchcredit.infrastructure.kafka;
 
 import br.com.searchcredit.infrastructure.kafka.event.ConsultaCreditoEvent;
+import br.com.searchcredit.infrastructure.kafka.event.SolicitacaoCreditoAnalisadaEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -9,16 +10,25 @@ import org.springframework.stereotype.Component;
 @Component
 public class KafkaEventPublisher {
     private static final Logger logger = LoggerFactory.getLogger(KafkaEventPublisher.class);
-    private static final String TOPIC = "consulta-creditos-topic";
+    private static final String TOPIC_CONSULTA = "consulta-creditos-topic";
 
-    private final KafkaTemplate<String, ConsultaCreditoEvent> kafkaTemplate;
+    private final KafkaTemplate<String, ConsultaCreditoEvent> consultaKafkaTemplate;
+    private final KafkaTemplate<String, SolicitacaoCreditoAnalisadaEvent> analiseKafkaTemplate;
 
-    public KafkaEventPublisher(KafkaTemplate<String, ConsultaCreditoEvent> kafkaTemplate) {
-        this.kafkaTemplate = kafkaTemplate;
+    public KafkaEventPublisher(
+            KafkaTemplate<String, ConsultaCreditoEvent> consultaKafkaTemplate,
+            KafkaTemplate<String, SolicitacaoCreditoAnalisadaEvent> analiseKafkaTemplate) {
+        this.consultaKafkaTemplate = consultaKafkaTemplate;
+        this.analiseKafkaTemplate = analiseKafkaTemplate;
     }
 
     public void publishConsultaCredito(ConsultaCreditoEvent event) {
-        kafkaTemplate.send(TOPIC, event);
-        logger.info("ConsultaCreditoEvent publicado para o tópico '{}': {}", TOPIC, event);
+        consultaKafkaTemplate.send(TOPIC_CONSULTA, event);
+        logger.info("ConsultaCreditoEvent publicado para o tópico '{}': {}", TOPIC_CONSULTA, event);
+    }
+
+    public void publishSolicitacaoAnalisada(SolicitacaoCreditoAnalisadaEvent event) {
+        analiseKafkaTemplate.send(TOPIC_CONSULTA, event);
+        logger.info("SolicitacaoCreditoAnalisadaEvent publicado para o tópico '{}': {}", TOPIC_CONSULTA, event);
     }
 }
