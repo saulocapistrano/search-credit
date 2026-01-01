295f652a6d40dd749a21db86eaa3f36029184b68
saulocapistrano <saulocapistrano@gmail.com>
2026-01-01 18:52:46 -0300

    feat(repository): add pagination support to CreditoRepository with new findAll method for pageable results

--- src/main/java/br/com/searchcredit/domain/repository/CreditoRepository.java
+++ src/main/java/br/com/searchcredit/domain/repository/CreditoRepository.java
@@ -1,6 +1,8 @@
 package br.com.searchcredit.domain.repository;
 
 import br.com.searchcredit.domain.entity.Credito;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,4 +12,6 @@ public interface CreditoRepository {
     Optional<Credito> findByNumeroCredito(String numeroCredito);
 
     List<Credito> findAllByNumeroNfse(String numeroNfse);
+
+    Page<Credito> findAll(Pageable pageable);
 }
