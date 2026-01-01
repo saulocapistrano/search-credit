a646814e192e11cd6b5e36babf07c7134152486b
saulocapistrano <saulocapistrano@gmail.com>
2026-01-01 19:41:11 -0300

    feat(repository): add pagination and status filtering methods to CreditoJpaRepository for improved credit queries

--- src/main/java/br/com/searchcredit/infrastructure/repository/jpa/CreditoJpaRepository.java
+++ src/main/java/br/com/searchcredit/infrastructure/repository/jpa/CreditoJpaRepository.java
@@ -1,6 +1,9 @@
 package br.com.searchcredit.infrastructure.repository.jpa;
 
 import br.com.searchcredit.domain.entity.Credito;
+import br.com.searchcredit.domain.enums.StatusCredito;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -10,5 +13,9 @@ public interface CreditoJpaRepository extends JpaRepository<Credito, Long> {
 
     Optional<Credito> findByNumeroCredito(String numeroCredito);
     List<Credito> findAllByNumeroNfse(String numeroNfse);
+
+    Page<Credito> findByNomeSolicitante(String nomeSolicitante, Pageable pageable);
+
+    List<Credito> findByStatus(StatusCredito status);
 
 }
