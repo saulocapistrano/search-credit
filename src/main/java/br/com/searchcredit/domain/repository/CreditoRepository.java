3c995b89eee7e88781c4c1655f40db5a0ce3485f
saulocapistrano <saulocapistrano@gmail.com>
2026-01-01 19:40:56 -0300

    feat(repository): add methods to CreditoRepository for saving credits, querying by applicant name, and filtering by status

--- src/main/java/br/com/searchcredit/domain/repository/CreditoRepository.java
+++ src/main/java/br/com/searchcredit/domain/repository/CreditoRepository.java
@@ -1,6 +1,7 @@
 package br.com.searchcredit.domain.repository;
 
 import br.com.searchcredit.domain.entity.Credito;
+import br.com.searchcredit.domain.enums.StatusCredito;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -9,9 +10,17 @@ import java.util.Optional;
 
 public interface CreditoRepository {
 
+    Credito save(Credito credito);
+
     Optional<Credito> findByNumeroCredito(String numeroCredito);
 
     List<Credito> findAllByNumeroNfse(String numeroNfse);
 
     Page<Credito> findAll(Pageable pageable);
+
+    Page<Credito> findByNomeSolicitante(String nomeSolicitante, Pageable pageable);
+
+    List<Credito> findByStatus(StatusCredito status);
+
+    Optional<Credito> findById(Long id);
 }
