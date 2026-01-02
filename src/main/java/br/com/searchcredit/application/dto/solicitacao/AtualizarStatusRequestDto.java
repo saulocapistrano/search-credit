a956f578eba78f7fd8d7250c533dc7fdc2e29390
saulocapistrano <saulocapistrano@gmail.com>
2026-01-02 08:18:07 -0300

    refactor(dto): change status field in AtualizarStatusRequestDto from StatusSolicitacao to StatusCredito for consistency in credit status representation

--- src/main/java/br/com/searchcredit/application/dto/solicitacao/AtualizarStatusRequestDto.java
+++ src/main/java/br/com/searchcredit/application/dto/solicitacao/AtualizarStatusRequestDto.java
@@ -1,6 +1,6 @@
 package br.com.searchcredit.application.dto.solicitacao;
 
-import br.com.searchcredit.domain.enums.StatusSolicitacao;
+import br.com.searchcredit.domain.enums.StatusCredito;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
@@ -8,6 +8,6 @@ import lombok.Data;
 public class AtualizarStatusRequestDto {
 
     @NotNull(message = "Status é obrigatório")
-    private StatusSolicitacao status;
+    private StatusCredito status;
 }
 
